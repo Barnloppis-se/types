5c107a0196180ded323165827dc5eed53bd83d5e
Xtarii <carlalvinelias@gmail.com>
2026-06-28 22:37:05 +0200

    updated storage type

--- data/item.ts
+++ data/item.ts
@@ -1,5 +1,5 @@
 import type { UUID } from "crypto"
-import type { Item } from "../upload/uploadData"
+import type { Item, TAGS } from "../upload/uploadData"
 
 /**
  * Item object type, see {@link Item}
@@ -25,7 +25,28 @@ export type ItemObject = {
     date: string,
 
     /**
-     * Item data
+     * Item tag
+     *
+     * The reason why it is marked,
+     * see {@link Types.Upload.TAGS}
      */
-    data: Item
+    tag: keyof typeof TAGS,
+
+    /**
+     * Item seller
+     *
+     * This may be null if the item
+     * misses a seller.
+     */
+    seller?: number,
+
+    /**
+     * Images of the item
+     *
+     * A list of images taken of
+     * the item. This may be more
+     * than one but should not be
+     * less than one.
+     */
+    images: string[]
 }
