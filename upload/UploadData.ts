9ad9d54551aba1b3cc4a36bd1ed221fcaededa2c
Xtarii <carlalvinelias@gmail.com>
2026-06-28 13:40:23 +0200

    moved out from namespace
    - Used to C#, well TS is not like C#

--- upload/UploadData.ts
+++ upload/UploadData.ts
@@ -1,64 +1,62 @@
-export namespace Types.Upload {
+/**
+ * Item error or anomaly.
+ *
+ * The reason why the item was or is
+ * being uploaded to the server.
+ */
+export enum TAGS {
+    "Omärkt", // This needs to be first
+
+    "Trasig",
+    "Fläckigt",
+    "Fel säsong",
+    "Slitet",
+    "Nopprigt",
+    "Felmärkt",
+    "Ej tillåten",
+    "Ej barn / ungdom",
+}
+
+/**
+ * Item tags list
+ *
+ * A list of all the tags available
+ * for items. See {@link Types.Upload.TAGS}
+ */
+export const tags = Object.keys(TAGS).filter(value => isNaN(Number(value)))
+
+
+
+/**
+ * Item type
+ *
+ * This represents the type used to describe
+ * an item that is in any way unfit to sell.
+ */
+export type Item = {
     /**
-     * Item error or anomaly.
+     * Item tag
      *
-     * The reason why the item was or is
-     * being uploaded to the server.
+     * The reason why it is marked,
+     * see {@link Types.Upload.TAGS}
      */
-    export enum TAGS {
-        "Omärkt", // This needs to be first
-
-        "Trasig",
-        "Fläckigt",
-        "Fel säsong",
-        "Slitet",
-        "Nopprigt",
-        "Felmärkt",
-        "Ej tillåten",
-        "Ej barn / ungdom",
-    }
+    tag: keyof typeof TAGS,
 
     /**
-     * Item tags list
+     * Item seller
      *
-     * A list of all the tags available
-     * for items. See {@link Types.Upload.TAGS}
+     * This may be null if the item
+     * misses a seller.
      */
-    export const tags = Object.keys(TAGS).filter(value => isNaN(Number(value)))
-
-
+    seller?: number,
 
     /**
-     * Item type
+     * Images of the item
      *
-     * This represents the type used to describe
-     * an item that is in any way unfit to sell.
+     * A list of images taken of
+     * the item. This may be more
+     * than one but should not be
+     * less than one.
      */
-    export type Item = {
-        /**
-         * Item tag
-         *
-         * The reason why it is marked,
-         * see {@link Types.Upload.TAGS}
-         */
-        tag: keyof typeof TAGS,
-
-        /**
-         * Item seller
-         *
-         * This may be null if the item
-         * misses a seller.
-         */
-        seller?: number,
-
-        /**
-         * Images of the item
-         *
-         * A list of images taken of
-         * the item. This may be more
-         * than one but should not be
-         * less than one.
-         */
-        images: Blob[]
-    }
+    images: Blob[]
 }
