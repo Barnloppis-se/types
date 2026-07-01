f7192360b60d58cb2965672b0f242f16d5d72f3a
Xtarii <carlalvinelias@gmail.com>
2026-07-01 19:17:48 +0200

    added response types
    - May get changed in the future to be more dynamic with each response

--- @types.ts
+++ @types.ts
@@ -1,2 +1,3 @@
 import * as upload from "./upload/uploadData"
 import * as item from "./data/item"
+import * as response from "./response/response"
---
+++ response/response.ts
@@ -0,0 +1,48 @@
+/**
+ * Barnloppis backend response type
+ */
+export interface Response {
+    /**
+     * Response status
+     *
+     * `True` of there was no error
+     * and `False` if there is an
+     * error from the server
+     */
+    status: boolean
+
+    /**
+     * Error cause
+     *
+     * This is `undefined` if `status` is `True`
+     * and thus the server did not respond with
+     * an error. Otherwise this contains the
+     * error cause message - the source of the
+     * error from the server.
+     */
+    cause?: string
+
+    /**
+     * Error message
+     *
+     * See {@link cause} as this uses the
+     * same logic for when the message
+     * is `undefined` and not.
+     */
+    message?: string
+}
+
+
+
+/**
+ * Barnloppis API item response
+ */
+export interface ItemResponse extends Response {
+    /**
+     * Item ID
+     *
+     * If the server responded without any errors
+     * this value is the ID of the item.
+     */
+    item?: string
+}
